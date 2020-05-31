341c09baa1df9524c2a0c6d17be3cb0c8748e4ca
Ben Loll <xTheCableGuyx@gmail.com>
2020-05-31 15:11:19 -0400

    Update MH: Warpath Wave Calculator.js

--- MH: Warpath Wave Calculator.js
+++ MH: Warpath Wave Calculator.js
@@ -2,32 +2,45 @@
 // @name         MH: Warpath Wave Calculator
 // @author       Warden Slayer - Warden Slayer#2302
 // @namespace    https://greasyfork.org/en/users/227259-wardenslayer
-// @version      1.1.2
+// @version      1.1.3
 // @description  Keeps track of remaining wave mice to help you manage the wave.
 // @include      https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js
 // @include      http://www.mousehuntgame.com/*
 // @include      https://www.mousehuntgame.com/*
 // ==/UserScript==
 $(document).ready(function() {
-    var observer = new MutationObserver(callback);
-    var observerOptions = {
+    const debug = localStorage.getItem('ws.debug');
+    if (debug == true) {
+        console.log('FW Script Started');
+    }
+    const observer = new MutationObserver(callback);
+    const observerOptions = {
         childList: true,
         attributes: false,
         subtree: false,
         characterData: false
     };
-    if ($(".hudLocationContent.desert_warpath").get(0)) {
-        if ($('.warpathHUD-streak-quantity').get(0)) {
-            observer.observe($('.warpathHUD-streak-quantity').get(0), observerOptions);
+    if ($('.warpathHUD-streak-quantity').get(0)) {
+        if (debug == true) {
+            console.log('Observing Streak');
         }
-        updateWave();
+        observer.observe($('.warpathHUD-streak-quantity').get(0), observerOptions);
+    } else {
+        if (debug == true) {
+            console.log('Streak not found');
+        }
     }
+    updateWave();
 });
 
 function callback(mutationList, observer) {
+    const debug = localStorage.getItem('ws.debug');
     mutationList.forEach(mutation => {
         switch (mutation.type) {
             case "childList":
+                if (debug == true) {
+                    console.log('Observer Triggered');
+                }
                 updateWave()
                 break;
         }
@@ -35,24 +48,28 @@ function callback(mutationList, observer) {
 }
 
 function updateWave() {
-    var wave = "";
-    var waveRetreat = "";
-    var waveMice = "";
-    var warrior = "";
-    var scout = "";
-    var archer = "";
-    var cavalry = "";
-    var mage = "";
-    var artillery = "";
-    var streakedW = 0;
-    var streakedS = 0;
-    var streakedA = 0;
-    var streakedC = 0;
-    var streakedM = 0;
-    var streakedF = 0;
-    var retreatText = "";
-    var resultString = "";
-    var streak = parseInt($('.warpathHUD-streak-quantity').text(), 10)
+    const debug = localStorage.getItem('ws.debug');
+    let wave = "";
+    let waveRetreat = "";
+    let waveMice = "";
+    let warrior = "";
+    let scout = "";
+    let archer = "";
+    let cavalry = "";
+    let mage = "";
+    let artillery = "";
+    let streakedW = 0;
+    let streakedS = 0;
+    let streakedA = 0;
+    let streakedC = 0;
+    let streakedM = 0;
+    let streakedF = 0;
+    let retreatText = "";
+    let resultString = "";
+    const streak = parseInt($('.warpathHUD-streak-quantity').text(), 10)
+    if (debug == true) {
+        console.log('Updating Wave...Current Streak:',streak);
+    }
     if ($('.warpathHUD.showPortal.wave_1').get(0)) {
         wave = 1;
         waveRetreat = 10;
@@ -67,6 +84,12 @@ function updateWave() {
         waveMice = $('.warpathHUD-wave.wave_3').children();
     } else {
         return
+    }
+    if (debug == true) {
+        console.log('Wave Retreat:',waveRetreat);
+    }
+    if (debug == true) {
+        console.log('Wave Mice:',waveMice);
     }
     if (waveMice.get(0)) {
         warrior = waveMice.get(0);
@@ -140,11 +163,14 @@ function updateWave() {
         remainingF = 0;
         streakedF = remainingF;
     }
-    var totalRemaining = 0 + remainingW + remainingS + remainingA + remainingC + remainingM + remainingF;
+    if (debug == true) {
+        console.log('Mouse Breakdown:',remainingW,remainingS,remainingA,remainingC,remainingM,remainingF);
+    }
+    const totalRemaining = 0 + remainingW + remainingS + remainingA + remainingC + remainingM + remainingF;
     const panicMeter = $('.warpathHUD-moraleBar.mousehuntTooltipParent');
-    var streaked = 0 + streakedW + streakedS + streakedA + streakedC + streakedM + streakedF;
+    const streaked = 0 + streakedW + streakedS + streakedA + streakedC + streakedM + streakedF;
     if (totalRemaining > waveRetreat) {
-        var retreatingIn = totalRemaining - waveRetreat;
+        const retreatingIn = totalRemaining - waveRetreat;
         if (retreatingIn > 1) {
             retreatText = retreatingIn + " catches left";
         } else {
@@ -165,7 +191,10 @@ function updateWave() {
             'text-align': 'center'
         })
     }
-    var highLow = (totalRemaining - streaked) - waveRetreat;
+    if (debug == true) {
+        console.log('Retreat Text:',retreatText);
+    }
+    let highLow = (totalRemaining - streaked) - waveRetreat;
     if (highLow > 0) {
         const plus = "+";
         highLow = plus.concat(highLow);
@@ -175,10 +204,13 @@ function updateWave() {
     } else {
         resultString = "Wave commander cutoff: " + waveRetreat + "\nMice left after commander: " + (totalRemaining - streaked) + " (" + highLow + ")";
     }
+    if (debug == true) {
+        console.log('Hover Text:',resultString);
+    }
     return resultString
 }
 $('.warpathHUD-moraleBar.mousehuntTooltipParent').mouseover(function() {
-    var title = updateWave();
+    const title = updateWave();
     $('.warpathHUD-moraleBar.mousehuntTooltipParent').attr('title', title);
     $('.warpathHUD-moraleBar.mousehuntTooltipParent').css('cursor', 'pointer');
 });
