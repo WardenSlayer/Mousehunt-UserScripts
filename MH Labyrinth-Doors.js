c80b34be29bc2f6af110a951e402e8d4900060f0
Ben Loll <xTheCableGuyx@gmail.com>
2022-07-12 22:36:25 -0400

    Update MH Labyrinth-Doors.js

--- MH Labyrinth-Doors.js
+++ MH Labyrinth-Doors.js
@@ -2,7 +2,7 @@
 // @name         MH: Labyrinth Door Data Collector
 // @author       Warden Slayer - Warden Slayer#2010
 // @namespace    https://greasyfork.org/en/users/227259-wardenslayer
-// @version      1.2.6
+// @version      1.2.7
 // @description  Mousehunt data collection tool for avilible labyrinth doors
 // @icon         https://www.mousehuntgame.com/images/items/weapons/974151e440f297f1b6d55385310ac63c.jpg?cv=2
 // @include      https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
             console.log('Labyrinth Door Script Running');
         }
         buildWorkingIndicator();
-        copyData();
+        copyDataNew();
     } else {
         if (debug == true) {
             console.log('Not in the Labyrinth');
@@ -28,7 +28,7 @@ $(document).ready(function() {
 $(document).ajaxStop(function(){
     if (user.environment_name == 'Labyrinth') {
         buildWorkingIndicator();
-        copyData();
+        copyDataNew();
     }
 });
 
@@ -44,10 +44,11 @@ function buildWorkingIndicator() {
     const workingIndicator = document.createElement("div");
     workingIndicator.classList.add("workingIndicator");
     $(workingIndicator).text('Collecting Door Data');
-    workingIndicator.addEventListener("click", copyData);
+    workingIndicator.addEventListener("click", copyDataNew);
     workingIndicatorContainer.appendChild(workingIndicator);
     $(workingIndicator).css({
         'margin-right': '5px',
+        'cursor': 'pointer',
     })
     hudLocation.after(workingIndicatorContainer);
     $('.workingIndicatorContainer').css({
@@ -59,35 +60,22 @@ function buildWorkingIndicator() {
     });
 }
 
-function copyData() {
+function copyDataNew() {
     const debug = localStorage.getItem('ws.debug');
-    const lastStep = $(".labyrinthHUD-hallway-padding").children().last();
-    if ($('.labyrinthHUD.intersection.entrance').get(0)) {
+    const status = user.quests.QuestLabyrinth.status;
+    if (status == 'intersection') {
         if (debug == true) {
-            console.log('At the Entrance. Submit cancelled')
-        }
-    } else if ($(lastStep).hasClass("labyrinthHUD-hallway-tile active") || $(lastStep).hasClass("labyrinthHUD-hallway-tile locked")) {
-        if (debug == true) {
-            console.log('Not in an intersection. Submit cancelled')
-        }
-        return;
-    } else {
-        if (debug == true) {
-            console.log('Collecting Data')
+            console.log('Collecting Data');
         }
         let resultsArray = [];
-        const allFactions = $('[class*="labyrinthHUD-clueDrawer-clue"]');
-        allFactions.each(function(i) {
-            const thisClueQty = parseInt($(this).find(".labyrinthHUD-clueDrawer-quantity").text(),10)
-            resultsArray[i] = thisClueQty;
-        });
-        const allDoors = $(".labyrinthHUD-doorContainer").children();
-        allDoors.each(function(i) {
-            const thisDoor = parseDoor(this);
-            resultsArray.push(thisDoor.doorLength,thisDoor.doorQuality,thisDoor.doorType)
-        });
-        if (debug == true) {
-            console.log(resultsArray);
+        const allFactions = user.quests.QuestLabyrinth.all_clues;
+        for (let key in allFactions) {
+            resultsArray.push(allFactions[key].quantity);
+        }
+        const allDoors = user.quests.QuestLabyrinth.doors;
+        for (let key in allDoors) {
+            const thisDoor = parseDoorNew(allDoors[key]);
+            resultsArray.push(thisDoor.doorLength,thisDoor.doorQuality,thisDoor.doorType);
         }
         //Add flag to denote shuffling
         const journalText = $(".journaltext");
@@ -97,22 +85,29 @@ function copyData() {
         const results = resultsArray.join();
         console.log('Result String: ', results);
         publishResults(results);
+    } else if (status == 'intersection enterance'){
+        if (debug == true) {
+            console.log('At the Entrance. Submit cancelled');
+            return;
+        }
+    } else {
+        if (debug == true) {
+            console.log('Not in an intersection. Submit cancelled');
+            return;
+        }
     }
 }
 
-function parseDoor(door) {
+function parseDoorNew(door) {
     const debug = localStorage.getItem('ws.debug');
-    if ($(door).hasClass('mystery')) {
-        return
-    };
-    const doorString = $(door).find(".labyrinthHUD-door-name-padding").text();
-    const doorArray = doorString.split(/\s+/);
+    if (debug == true) {
+        console.log("Door before shortening: ", door.name)
+    }
+    const doorString = door.name.replace(" Hallway","");
+    const doorArray = doorString.split(" ");
     let doorLength = doorArray[0];
     let doorQuality = doorArray[1];
     let doorType = doorArray[2];
-    if (debug == true) {
-        console.log("Door before shortening: ", doorLength, doorQuality, doorType)
-    }
     //shorten door length
     if (doorLength == "Long") {
         doorLength = "L";
