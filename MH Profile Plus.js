2be00ced147ffdc443e354cb1d665b491dac63d2
Brad Parbs <brad@bradparbs.com>
2023-03-22 12:18:15 -0500

    Bump version

--- MH Profile Plus.js
+++ MH Profile Plus.js
@@ -2,7 +2,7 @@
 // @name         MH: Profile+
 // @author       Warden Slayer - Warden Slayer#2010
 // @namespace    https://greasyfork.org/en/users/227259-wardenslayer
-// @version      1.26
+// @version      1.27
 // @description  Community requested features for the tabs on your MH profile.
 // @grant        GM_xmlhttpRequest
 // @icon         https://www.mousehuntgame.com/images/items/weapons/974151e440f297f1b6d55385310ac63c.jpg?cv=2
